8611a469737f8d282d1aadf60983955fefd832c3
Maxime-DW <maxime.joly54@gmail.com>
2022-08-17 20:41:09 +0200

    php

---
+++ mail.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Envoi d'un message par formulaire</title>
+</head>
+
+<body>
+    <?php
+    $retour = mail('destinataire@free.fr', 'Envoi depuis la page Contact', $_POST['message'], '');
+    if ($retour)
+        echo '<p>Votre message a bien été envoyé.</p>';
+    ?>
+</body>
+
+</html>
